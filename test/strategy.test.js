14eb13179b9c0b18fabae4128ec37e3b6f8d6518
Max Stoliar <hexelon@gmail.com>
2017-11-22 15:04:40 +0200

    rename domain to auth0domain so we're consistent with loopback

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -5,7 +5,7 @@ var should = require('should');
 describe('auth0 strategy', function () {
   before(function () {
     this.strategy = new Auth10Strategy({
-       domain:       'jj.auth0.com', 
+       auth0Domain:  'jj.auth0.com',
        clientID:     'testid',
        clientSecret: 'testsecret',
        callbackURL:  '/callback'
@@ -18,12 +18,12 @@ describe('auth0 strategy', function () {
     this.strategy.options
       .authorizationURL.should.eql('https://jj.auth0.com/authorize');
   });
-  
+
   it('tokenURL should have the domain', function () {
     this.strategy.options
       .tokenURL.should.eql('https://jj.auth0.com/oauth/token');
   });
-  
+
   it('userInfoURL should have the domain', function () {
     this.strategy.options
       .userInfoURL.should.eql('https://jj.auth0.com/userinfo');
@@ -73,9 +73,9 @@ describe('auth0 strategy', function () {
 
   });
 
-  describe('authenticate', function () { 
+  describe('authenticate', function () {
     it('when there is an error querystring propagate', function (done) {
-      
+
       this.strategy.fail = function (challenge, status) {
         challenge.should.eql('domain_mismatch');
         done();
@@ -87,5 +87,5 @@ describe('auth0 strategy', function () {
         }
       });
     });
-  }); 
+  });
 });
